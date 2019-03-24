f70e2454e979807d928962ca9a2a2a6a505f129e
mukhlashin <abdullahmukhalshiin@gmail.com>
2019-03-24 15:04:14 +0700

    Inital commit

--- app/src/main/java/com/example/sportsapp/Splash.java
+++ app/src/main/java/com/example/sportsapp/Splash.java
@@ -1,13 +1,41 @@
 package com.example.sportsapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class Splash extends AppCompatActivity {
+
+    TextView tvSplash;
+    ImageView imgSplash;
+    GifImageView gifSplash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tvSplash = findViewById(R.id.tv_splash);
+        imgSplash = findViewById(R.id.img_splash);
+        gifSplash = findViewById(R.id.gif_splash);
+
+        final Intent i = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
